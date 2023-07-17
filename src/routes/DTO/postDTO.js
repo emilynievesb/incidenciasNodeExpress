f503e512ffbee55845cc7acec361af8539bae45b
emilynievesb <nemily1710@gmail.com>
2023-07-16 22:06:58 -0500

    feat: :sparkles: DTO con YUP
    Se realizó el DTO para la validación del body cuando se agregara un nuevo trainer

--- src/routes/DTO/postDTO.js
+++ src/routes/DTO/postDTO.js
@@ -38,5 +38,61 @@ const postEquipoDTO = async (req, res, next) => {
     res.status(400).json({ status: "fail", message: error.errors });
   }
 };
+const postTrainerDTO = async (req, res, next) => {
+  try {
+    const productSchema = object({
+      nombre: string()
+        .strict()
+        .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Is not in correct format")
+        .required(),
+      emailPersonal: string().required(),
+      emailCorporativo: string().required(),
+      telefonoMovil: number()
+        .required()
+        .test("length", "El teléfono movil debe tener 10 dígitos", (value) => {
+          if (!value) return true; // Si el campo está vacío, no realizar la validación
+          const numberString = String(value);
+          return numberString.length === 10;
+        }),
+      telfonoResidencia: number()
+        .required()
+        .test(
+          "length",
+          "El telefono de la residencia debe tener 10 dígitos",
+          (value) => {
+            if (!value) return true; // Si el campo está vacío, no realizar la validación
+            const numberString = String(value);
+            return numberString.length === 10;
+          }
+        ),
+      telefonoEmpresa: number()
+        .required()
+        .test(
+          "length",
+          "El telefono de la empresa debe tener 10 dígitos",
+          (value) => {
+            if (!value) return true; // Si el campo está vacío, no realizar la validación
+            const numberString = String(value);
+            return numberString.length === 10;
+          }
+        ),
+      telefonoMovilEmpresa: number()
+        .required()
+        .test(
+          "length",
+          "El telefono movil de la empresa debe tener 10 dígitos",
+          (value) => {
+            if (!value) return true; // Si el campo está vacío, no realizar la validación
+            const numberString = String(value);
+            return numberString.length === 10;
+          }
+        ),
+    });
+    await productSchema.validate(req.body);
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.errors });
+  }
+};
 
-export { postInsidenciaDTO, postEquipoDTO };
+export { postInsidenciaDTO, postEquipoDTO, postTrainerDTO };
--- src/routes/postData.js
+++ src/routes/postData.js
@@ -4,7 +4,11 @@ import {
   postEquipoController,
   postTrainerController,
 } from "../controllers/postDataCampus.js";
-import { postInsidenciaDTO, postEquipoDTO } from "./DTO/postDTO.js";
+import {
+  postInsidenciaDTO,
+  postEquipoDTO,
+  postTrainerDTO,
+} from "./DTO/postDTO.js";
 const postInitRoute = () => {
   const router = Router();
   router.post(
@@ -13,7 +17,7 @@ const postInitRoute = () => {
     postInsidenciaController
   );
   router.post("/agregarEquipo", postEquipoDTO, postEquipoController);
-  router.post("/agregarTrainer", postTrainerController);
+  router.post("/agregarTrainer", postTrainerDTO, postTrainerController);
   return router;
 };
 
